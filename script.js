7c02493f6442df03b9f48874c1d0e08862e59759
Mitchell Robles <mitchellr92@live.com>
2021-11-01 10:14:34 -0700

    victory sound when completing puzzle

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ let END_TIME = null;
 let POP_SOUND = new Audio("./sounds/pop.mp3");
 POP_SOUND.volume = 0.5;
 
+let AUDIO_CONTEXT = new (AudioContext ||
+  webkitAudioContext ||
+  window.webkitAudioContext)();
+
+let keys = {
+  DO: 261.6,
+  RE: 293.7,
+  MI: 329.6
+};
+
 function main() {
   CANVAS = document.getElementById("myCanvas");
   CONTEXT = CANVAS.getContext("2d");
@@ -152,6 +162,7 @@ function onMouseUp() {
     if (isComplete() && END_TIME == null) {
       let now = new Date().getTime();
       END_TIME = now;
+      setTimeout(playMelody, 500);
     }
   }
   SELECTED_PIECE = null;
@@ -278,4 +289,39 @@ function distance(p1, p2) {
   return Math.sqrt(
     (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)
   );
+}
+
+function playNote(key, duration) {
+  let osc = AUDIO_CONTEXT.createOscillator();
+  osc.frequency.value = key;
+  osc.start(AUDIO_CONTEXT.currentTime);
+  osc.stop(AUDIO_CONTEXT.currentTime + duration / 1000);
+
+  let envelope = AUDIO_CONTEXT.createGain();
+  osc.connect(envelope);
+  osc.type = "triangle";
+  envelope.connect(AUDIO_CONTEXT.destination);
+  envelope.gain.setValueAtTime(0, AUDIO_CONTEXT.currentTime);
+  envelope.gain.linearRampToValueAtTime(0.5, AUDIO_CONTEXT.currentTime + 0.1);
+  envelope.gain.linearRampToValueAtTime(
+    0,
+    AUDIO_CONTEXT.currentTime + duration / 1000
+  );
+
+  setTimeout(function() {
+    osc.disconnect();
+  }, duration);
+}
+
+function playMelody() {
+  playNote(keys.MI, 300);
+  setTimeout(function() {
+    playNote(keys.DO, 300);
+  }, 300);
+  setTimeout(function() {
+    playNote(keys.RE, 150);
+  }, 450);
+  setTimeout(function() {
+    playNote(keys.MI, 600);
+  }, 600);
 }
